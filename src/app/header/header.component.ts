4460c68dee54f1999e47eeaf0cb9101487968dca
Ejyeyang <ejyeyang@gmail.com>
2021-03-30 17:08:34 -0400

    added an eventEmmitter to the appheader component so that user can choose to see ingredients or recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+  // @Output lets featureSelected be 'emitted' to the parent component which is the app component in this case
+  @Output() featureSelected = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // emits the string passed in from client side when button is clicked.
+  onSelect(feature: string){
+    this.featureSelected.emit(feature);
   }
 
 }
