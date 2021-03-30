ed7b57f9fce5e4fb322125df55a2e335c96531ed
Ejyeyang <ejyeyang@gmail.com>
2021-03-30 16:12:26 -0400

    added to the recipeDetail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,6 +14,16 @@ export class RecipeListComponent implements OnInit {
       "A test recipe",
       "testing",
       "https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+    ),
+    new Recipe(
+      "A test recipe",
+      "testing",
+      "https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+    ),
+    new Recipe(
+      "A test recipe",
+      "testing",
+      "https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
     )
   ];
   constructor() { }
