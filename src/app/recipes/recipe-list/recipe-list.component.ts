dfbe3cc3165cdf43219ec4f0a82af1de509449a3
Ejyeyang <ejyeyang@gmail.com>
2021-03-30 16:05:49 -0400

    string interpolation with testing data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    //testing recipe data
+    new Recipe(
+      "A test recipe",
+      "testing",
+      "https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+    )
+  ];
   constructor() { }
 
   ngOnInit(): void {
