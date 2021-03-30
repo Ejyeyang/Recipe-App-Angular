ac974caa3415cd357b1aed3771f92af9aa1a06bb
Ejyeyang <ejyeyang@gmail.com>
2021-03-30 16:26:32 -0400

    added an ingredient model and some test data to show in the client side of ingredient list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient(
+      "apples",
+      5
+    ),
+    new Ingredient(
+      "strawberries",
+      10
+    )
+  ];
   constructor() { }
 
   ngOnInit(): void {
