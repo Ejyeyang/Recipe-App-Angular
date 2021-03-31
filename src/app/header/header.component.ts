870184c908294e24e588b02cb8253b5a6cbb9667
Ejyeyang <ejyeyang@gmail.com>
2021-03-31 16:33:29 -0400

    used routes to go from recipes to shoppinglist components, got rid of alot of href tags to stop the page from reloading when click on dropdowns

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,17 +8,9 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
-  // @Output lets featureSelected be 'emitted' to the parent component which is the app component in this case
-  @Output() featureSelected = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
-  // emits the string passed in from client side when button is clicked.
-  onSelect(feature: string){
-    this.featureSelected.emit(feature);
-  }
-
 }
