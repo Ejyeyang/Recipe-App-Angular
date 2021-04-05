cee692abe21d24ff30b4e5d4a51ec443768e2073
Ejyeyang <ejyeyang@gmail.com>
2021-04-05 14:18:31 -0400

    ingredient add, edit delete form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { ShoppingListService } from '../../services/shopping-list.service';
 import { Ingredient } from '../../shared/ingredient.model';
 
@@ -8,28 +10,55 @@ import { Ingredient } from '../../shared/ingredient.model';
   styles: [
   ]
 })
-export class ShoppingEditComponent implements OnInit {
-  /**
-   * There is no way to define a decorator such that Typescript will know it will perform
-   * field initialization. The only option is to add a definite assignment modifier to the field:
-   * @ViewChild('nameInput') nameInputRef!: ElementRef;
-   * @ViewChild('amountInput') amountInputRef!: ElementRef; ERROR vs below
-   */
-  @ViewChild('nameInput') nameInputRef!: ElementRef;
-  @ViewChild('amountInput') amountInputRef!: ElementRef;
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f', {static: false}) slForm: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(
     private shoppingListService: ShoppingListService
   ) { }
 
   ngOnInit(): void {
+    this.subscription = this.shoppingListService.startedEditing.subscribe((index: number) =>{
+      this.editMode = true;
+      this.editedItemIndex = index;
+      this.editedItem = this.shoppingListService.getIngredient(index);
+      this.slForm.setValue({
+        name: this.editedItem.name,
+        amount: this.editedItem.amount
+      });
+    }, error => {
+      console.log(error);
+    });
   }
 
-  onAddItem(){
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
-    this.shoppingListService.addIngredient(newIngredient);
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
+  onSubmit(form: NgForm){
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    if(this.editMode){
+      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.shoppingListService.addIngredient(newIngredient);
+    }
+    this.editMode = false;
+    form.reset();
+  }
+
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete(){
+    this.shoppingListService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
   }
 
 }
