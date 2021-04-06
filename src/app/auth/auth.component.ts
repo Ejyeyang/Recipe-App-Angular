68efcb4850ed05613847321925c6b34f310d181f
Ejyeyang <ejyeyang@gmail.com>
2021-04-06 10:49:10 -0400

    added error handling for email existing already by pipe response for database

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -40,9 +40,9 @@ export class AuthComponent implements OnInit {
       this.authService.signup(email, password).subscribe(resData => {
         console.log(resData);
         this.isLoading = false;
-      }, error => {
-        console.log(error);
-        this.error = 'an error occured';
+      }, errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
         this.isLoading = false;
       });
     }
