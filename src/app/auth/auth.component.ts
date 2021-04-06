146868cc0aa773a9972c68f3a525b170aa679e07
Ejyeyang <ejyeyang@gmail.com>
2021-04-06 09:21:07 -0400

    added the submit button for the auth form, added the onsubmit functionality to console log and to reset form after the submit

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -17,6 +18,11 @@ export class AuthComponent implements OnInit {
   onSwitchMode(){
     //switchs from true to false to true to false whenever called
     this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form.value);
+    form.reset();
   }
 
 }
