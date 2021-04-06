1b3e3ac2f506eafe8b6252fd8345d9c4bb3f3dbe
Ejyeyang <ejyeyang@gmail.com>
2021-04-06 10:39:29 -0400

    added a little error handling and also a loading spinner component to be called when something is loading to show user its loading

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -10,6 +10,8 @@ import { AuthService } from '../services/auth.service';
 })
 export class AuthComponent implements OnInit {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
   constructor(
     private authService: AuthService
@@ -30,13 +32,18 @@ export class AuthComponent implements OnInit {
     const email = form.value.email;
     const password = form.value.password;
 
+    this.isLoading = true;
+
     if(this.isLoginMode){
       //...
     } else {
       this.authService.signup(email, password).subscribe(resData => {
         console.log(resData);
+        this.isLoading = false;
       }, error => {
         console.log(error);
+        this.error = 'an error occured';
+        this.isLoading = false;
       });
     }
     form.reset();
