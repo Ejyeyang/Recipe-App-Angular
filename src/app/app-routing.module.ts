17342e6557d93bf50af63b94a9b13d96869368e1
Ejyeyang <ejyeyang@gmail.com>
2021-04-07 20:45:38 -0400

    added a auth.guard to guard our routes with the user token login, now even if user writes the path in the url it will be redirected to the auth component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { AuthGuard } from './services/auth.guard';
 import { RecipeResolverService } from './services/recipe-resolver.service';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'recipes', component: RecipesComponent,
+  {path: 'recipes', component: RecipesComponent, canActivate:[AuthGuard],
   children:[
     {path: '', component: RecipeStartComponent},
     {path: 'new', component: RecipeEditComponent}, // order matters
